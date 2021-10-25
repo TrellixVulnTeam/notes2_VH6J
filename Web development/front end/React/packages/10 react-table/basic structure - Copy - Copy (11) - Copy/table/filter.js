cc1351a9b76c45fb4e17e0aa597497e0ec60b50c
Mamunur Rashid <40039219+mamun4023@users.noreply.github.com>
2021-10-26 00:52:39 +0600

    Add files via upload

---
+++ Web development/front end/React/packages/10 react-table/basic structure - Copy - Copy (11) - Copy/table/filter.js
@@ -0,0 +1,12 @@
+
+
+function Filter({filter, setFilter}){
+
+    return(
+        <>
+            <input value = {filter || "" } onChange = {(e)=> setFilter(e.target.value)} />
+        </>
+    )
+}
+
+export default Filter;
